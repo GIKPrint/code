683e1243fafa8784b6bb934f9704ff7dcca42b38
GIKPrint <cs.gikprint@gmail.com>
2023-08-10 16:33:03 +0700

    Update geeika.js

--- geeika.js
+++ geeika.js
@@ -1,10 +1,3 @@
-document.getElementByClassName("cek").onclick = function() {
-  alert(localStorage.getItem("idBerat"));
-  };
-  
-document.getElementByClassName("item_add").onclick = function(){simpanBerat()};
-  
-function simpanBerat(){
-  var item_berat = document.getElementByClassName("item_berat").innerText;  
-  localStorage.setItem("idBerat", item_berat);
-  }
+$(document).ready(function(){
+  alert('js berfungsi');
+});
